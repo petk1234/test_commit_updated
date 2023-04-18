833435a3b7cc87ca291db697a5e160fb322a2f14
petk1234 <petkun8034@gmail.com>
2023-04-18 18:17:14 +0300

    second

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <p>sjdksjdskjdkdf</p>
       </header>
     </div>
   );
