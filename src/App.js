f034cd1a876bc7c51b93b94f7276f7199eccff44
petk1234 <petkun8034@gmail.com>
2023-04-18 18:52:56 +0300

    lalala2 second

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>lalala2</p>
+        <p>lalala23</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
